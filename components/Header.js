e5fa6a4354a5bb405d91bbdcc7c22c34791565a7
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2022-10-04 15:00:06 +0900

    로고 CSS 적용

--- components/Header.js
+++ components/Header.js
@@ -70,7 +70,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
           <Link href="/">
             <a aria-label={BLOG.title}>
               <div className="h-6">
-                <img src="/logo.png" style="width:24px" />
+                <img src="/logo.png" className="logo_img" />
               </div>
             </a>
           </Link>
