b4db76b4ee6a9c602017376c522eb6ba1f2ea531
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2023-05-30 07:46:44 +0900

    Set dev env

--- blog.config.js
+++ blog.config.js
@@ -56,7 +56,7 @@ const BLOG = {
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
   },
-  isProd: true // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  isProd: false // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
 }
 // export default BLOG
 module.exports = BLOG
