1d715eb99d284fe6b9b111290fc5cc2c4ab69f0f
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2026-04-25 11:28:29 +0900

    Adjust postsPerPage setting in blog.config.js
    Updated postsPerPage from 7 to 5 in blog configuration.

--- blog.config.js
+++ blog.config.js
@@ -4,27 +4,27 @@ const BLOG = {
   email: 'yumeka@occm.cc',
   link: 'https://blog.occm.cc',
   description: '자커마스에 대해 다루는 블로그입니다.',
-  lang: 'ko-KR', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'ko-KR',
   timezone: 'Asia/Seoul',
-  appearance: 'dark', // ['light', 'dark', 'auto'],
-  font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#18181B', // use hex value, don't forget '#'
-  path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  appearance: 'dark',
+  font: 'sans-serif',
+  lightBackground: '#ffffff',
+  darkBackground: '#18181B',
+  path: '',
+  since: 2021,
+  postsPerPage: 5,
   sortByDate: true,
   showAbout: true,
   showArchive: true,
-  autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://occm-og.vercel.app', // The link to generate OG image, don't end with a slash
+  autoCollapsedNavBar: false,
+  ogImageGenerateURL: 'https://occm-og.vercel.app',
   socialLink: 'https://occm.cc/@yumeka',
   seo: {
     keywords: ['자커마스', '마스토돈', 'OCCM'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+    googleSiteVerification: ''
   },
-  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
-  notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
+  notionPageId: process.env.NOTION_PAGE_ID,
+  notionAccessToken: process.env.NOTION_ACCESS_TOKEN,
   analytics: {
     provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
@@ -37,10 +37,9 @@ const BLOG = {
     }
   },
   comment: {
-    // support provider: gitalk, utterances, cusdis
-    provider: 'cusdis', // leave it empty if you don't need any comment plugin
+    provider: 'cusdis',
     gitalkConfig: {
-      repo: '', // The repository of store comments
+      repo: '',
       owner: '',
       admin: [],
       clientID: '',
@@ -51,12 +50,12 @@ const BLOG = {
       repo: 'Lastorder-DC/blog.occm.cc'
     },
     cusdisConfig: {
-      appId: 'c8e56656-7e65-4bdf-a3f6-6efb89dc288b', // data-app-id
-      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
-      scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
+      appId: 'c8e56656-7e65-4bdf-a3f6-6efb89dc288b',
+      host: 'https://cusdis.com',
+      scriptSrc: 'https://cusdis.com/js/cusdis.es.js'
     }
   },
-  isProd: true // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  isProd: true
 }
 // export default BLOG
 module.exports = BLOG
